c6a848ad5177e72bacb8a543c3f3551386cdb8a7
Fabien PONCET <fponcet@centumadeneo.com>
2026-03-31 15:16:22 +0200

    Correction SEO

--- src/js/common/boutons.js
+++ src/js/common/boutons.js
@@ -4,7 +4,7 @@ import { Observer } from 'tailwindcss-intersect';
 import { loadComponent } from '/src/js/common/components.js'
 
 export async function InitBoutons() {
-    await loadComponent('#boutons', '/src/components/boutons.html')
+    await loadComponent('.boutons', '/src/components/boutons.html')
     
     Observer.start();   
 
